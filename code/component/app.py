a062ec06146043ac10f5a317e36b557eb8904a82
Akshay Verma <44718434+averma1010@users.noreply.github.com>
2024-03-25 11:37:09 -0400

    Update app.py

--- code/component/app.py
+++ code/component/app.py
@@ -4,35 +4,47 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from pyvis.network import Network
 import network
+import Charts_dashboard
 import numpy as np
 from datetime import datetime, timedelta
 st.set_page_config(layout="wide")
 
 st.title('Hate Speech Network Dashboard')
 
+
+
 c_left, c_right = st.columns(2)
 with c_left:
-    physics = st.checkbox('Add Physics')
-    start_date, end_date = st.slider("Select Date Range", 
+    physics_checkbox = st.sidebar.checkbox('Add Physics')
+
+    start_date, end_date = st.sidebar.slider("Select Date Range", 
                                      min_value=datetime(2021, 1, 1),
                                      max_value=datetime(2021, 1, 10),
                                      value=(datetime(2021, 1, 1), datetime(2021, 1, 5)),
                                      format="YYYY-MM-DD")
-    network.pre_event(physics, start_date, end_date)
-    HtmlFile = open("gameofthrones.html", 'r', encoding='utf-8')
+    network.pre_event(physics_checkbox, start_date, end_date)
+    HtmlFile = open("preevent.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read() 
+    components.html(source_code, height = 1200,width=750)
+
+    slider2_key = "slider2"
+    
+    start_date_1, end_date_1 = st.sidebar.slider("Select Date Range", 
+                                        min_value=datetime(2021, 1, 1),
+                                        max_value=datetime(2021, 1, 10),
+                                        value=(datetime(2021, 1, 6), datetime(2021, 1, 10)),
+                                        format="YYYY-MM-DD", key=slider2_key)
+    network.post_event(physics_checkbox, start_date_1, end_date_1)
+    HtmlFile = open("postevent.html", 'r', encoding='utf-8')
     source_code = HtmlFile.read() 
     components.html(source_code, height = 1200,width=750)
     
 
 with c_right:    
-    slider2_key = "slider2"
-    physics = st.checkbox('Add Physics', key='physics_checkbox')
-    start_date_1, end_date_1 = st.slider("Select Date Range", 
-                                        min_value=datetime(2021, 1, 1),
-                                        max_value=datetime(2021, 1, 10),
-                                        value=(datetime(2021, 1, 6), datetime(2021, 1, 10)),
-                                        format="YYYY-MM-DD", key=slider2_key)
-    network.post_event(physics, start_date_1, end_date_1)
-    HtmlFile = open("postevent.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    components.html(source_code, height = 1200,width=750)
+    
+
+
+    Charts_dashboard.edge_count_chart(start_date, end_date_1)
+
+
+    Charts_dashboard.network_comparison(start_date, end_date, start_date_1, end_date_1)
