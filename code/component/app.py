5e23f98fe9605ef1f9ebb954abfa03f5a7645ba2
Akshay Verma <44718434+averma1010@users.noreply.github.com>
2024-03-24 23:00:17 -0400

    Update app.py

--- code/component/app.py
+++ code/component/app.py
@@ -6,7 +6,7 @@ from pyvis.network import Network
 import network
 import numpy as np
 from datetime import datetime, timedelta
-
+st.set_page_config(layout="wide")
 
 st.title('Hate Speech Network Dashboard')
 
@@ -16,12 +16,23 @@ with c_left:
     start_date, end_date = st.slider("Select Date Range", 
                                      min_value=datetime(2021, 1, 1),
                                      max_value=datetime(2021, 1, 10),
-                                     value=(datetime(2021, 1, 1), datetime(2021, 1, 10)),
+                                     value=(datetime(2021, 1, 1), datetime(2021, 1, 5)),
                                      format="YYYY-MM-DD")
-    network.got_func(physics, start_date, end_date)
+    network.pre_event(physics, start_date, end_date)
     HtmlFile = open("gameofthrones.html", 'r', encoding='utf-8')
     source_code = HtmlFile.read() 
-    components.html(source_code, height = 1200,width=1000)
+    components.html(source_code, height = 1200,width=750)
     
 
-    
+with c_right:    
+    slider2_key = "slider2"
+    physics = st.checkbox('Add Physics', key='physics_checkbox')
+    start_date_1, end_date_1 = st.slider("Select Date Range", 
+                                        min_value=datetime(2021, 1, 1),
+                                        max_value=datetime(2021, 1, 10),
+                                        value=(datetime(2021, 1, 6), datetime(2021, 1, 10)),
+                                        format="YYYY-MM-DD", key=slider2_key)
+    network.post_event(physics, start_date_1, end_date_1)
+    HtmlFile = open("postevent.html", 'r', encoding='utf-8')
+    source_code = HtmlFile.read() 
+    components.html(source_code, height = 1200,width=750)
