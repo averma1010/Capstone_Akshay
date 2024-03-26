12d107822c3fa7b36065cb30c0b73085375e11ba
Akshay Verma <44718434+averma1010@users.noreply.github.com>
2024-03-26 10:01:51 -0400

    Update app.py

--- code/component/app.py
+++ code/component/app.py
@@ -7,13 +7,17 @@ import network
 import Charts_dashboard
 import numpy as np
 from datetime import datetime, timedelta
+#import RAG_Insights
+
+
+
 st.set_page_config(layout="wide")
+
 
 st.title('Hate Speech Network Dashboard')
 
-
-
 c_left, c_right = st.columns(2)
+
 with c_left:
     physics_checkbox = st.sidebar.checkbox('Add Physics')
 
@@ -22,10 +26,8 @@ with c_left:
                                      max_value=datetime(2021, 1, 10),
                                      value=(datetime(2021, 1, 1), datetime(2021, 1, 5)),
                                      format="YYYY-MM-DD")
-    network.pre_event(physics_checkbox, start_date, end_date)
-    HtmlFile = open("preevent.html", 'r', encoding='utf-8')
-    source_code = HtmlFile.read() 
-    components.html(source_code, height = 1200,width=750)
+   # network.pre_event(physics_checkbox, start_date, end_date)
+    
 
     slider2_key = "slider2"
     
@@ -34,11 +36,22 @@ with c_left:
                                         max_value=datetime(2021, 1, 10),
                                         value=(datetime(2021, 1, 6), datetime(2021, 1, 10)),
                                         format="YYYY-MM-DD", key=slider2_key)
-    network.post_event(physics_checkbox, start_date_1, end_date_1)
+   # network.post_event(physics_checkbox, start_date_1, end_date_1)
+    st.markdown("## Pre-event")
+
+    network.network_vis(physics_checkbox,start_date,end_date,start_date_1,end_date_1)
+    HtmlFile = open("preevent.html", 'r', encoding='utf-8')
+
+
+    source_code = HtmlFile.read() 
+    components.html(source_code, height = 500,width=750)
+    st.markdown("## Post-event")
+
     HtmlFile = open("postevent.html", 'r', encoding='utf-8')
     source_code = HtmlFile.read() 
     components.html(source_code, height = 1200,width=750)
     
+
 
 with c_right:    
     
@@ -48,3 +61,8 @@ with c_right:
 
 
     Charts_dashboard.network_comparison(start_date, end_date, start_date_1, end_date_1)
+
+    
+#RAG_Insights.rag_openai()
+    
+    
