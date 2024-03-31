bf084d1df08c433ccbec31057e1ede2590e2594b
Akshay Verma <44718434+averma1010@users.noreply.github.com>
2024-03-31 15:03:06 -0400

    Update Charts_dashboard.py

--- code/component/Charts_dashboard.py
+++ code/component/Charts_dashboard.py
@@ -20,10 +20,20 @@ from scipy.ndimage.filters import gaussian_filter1d
 from matplotlib.dates import DateFormatter, DayLocator
 import streamlit as st
 
+
+def load_data():
+    """
+    Load data from CSV file and perform necessary preprocessing.
+    """
+    file_path = r"C:\Users\Akshay\OneDrive\Desktop\Capstone_Akshay\code\component\Jan6.csv"
+    data = pd.read_csv(file_path)
+    data['Day'] = pd.to_datetime(data['Day'])
+    return data
+
 class EdgeCountAnalyzer:
     def __init__(self):
-        self.data = pd.read_csv(r"C:\Users\Akshay\OneDrive\Desktop\Capstone_Akshay\code\component\Jan6.csv")
-        self.data['Day'] = pd.to_datetime(self.data['Day'])
+        self.data = load_data()
+
 
     def calculate_relative_increase(self, start_date, end_date):
         # Converting to Datetime format
@@ -45,7 +55,7 @@ class EdgeCountAnalyzer:
 
         fig, ax = plt.subplots(figsize=(10, 6))
         ax.plot(index_values, chart_values, marker='o', linestyle='-', alpha=0.7, markersize=6)
-        ax.set_title('Increase in Hate Links Relative to Jan 1, 2021')
+        ax.set_title(f'Increase in Hate type relative to {start_date}')        
         ax.set_xlabel('Day')
         ax.set_ylabel('Increase in Number of Hate Links (%)')
         ax.tick_params(axis='x', rotation=45)
@@ -57,8 +67,8 @@ class EdgeCountAnalyzer:
 
 class Network_comparison:
     def __init__(self):
-        self.data = pd.read_csv(r"C:\Users\Akshay\OneDrive\Desktop\Capstone_Akshay\code\component\Jan6.csv")
-        self.data['Day'] = pd.to_datetime(self.data['Day'])
+        self.data = load_data()
+
 
 
     def network_comparison_metrics(self,start_date1, end_date1, start_date2, end_date2):
@@ -109,4 +119,102 @@ class Network_comparison:
         df = pd.DataFrame(data)
 
         st.write(df)
+
+
+class hate_line_plot:
+    def __init__(self) -> None:
+        self.data = load_data()
+
+    
+    def hate_type_relative_increase(self, start_date, end_date):
+        Jan6 = self.data[(self.data['Day'] >= start_date) & (self.data['Day'] <= end_date)]
+
+        boolean_columns = [ 'race_prediction', 'giso_prediction', 'immigration_prediction']
+        boolean_df = Jan6[['Day'] + boolean_columns]
+
+        # Assuming 'day' is the column containing the day information
+        first_day = boolean_df['Day'].iloc[0]
+
+        # Calculate the increase in true values relative to the first day for each boolean column
+        increase = boolean_df.groupby('Day').sum() - boolean_df[boolean_df['Day'] == first_day].sum()
+
+        # Reset index to make 'day' a column again
+        increase.reset_index(inplace=True)
+
+        # Plotting
+        fig, ax = plt.subplots(figsize=(10, 6))
+        for col in boolean_columns:
+            ax.plot(increase['Day'], increase[col], label=col)
+
+        ax.set_title(f'Increase in Hate type relative to {start_date}')
+        ax.set_xlabel('Day')
+        ax.set_ylabel('Increase')
+        ax.legend()
+        ax.grid(True)
+        st.pyplot(fig)
+
+        
+class corr_plot:
+    def __init__(self) -> None:
+        self.data = load_data()
+
+    def corr_plot(self, start_date, end_date):
+        Jan6 = self.data[(self.data['Day'] >= start_date) & (self.data['Day'] <= end_date)]
+
+        prediction_columns = ['religion_prediction', 'race_prediction', 'gender_prediction',
+                      'giso_prediction', 'immigration_prediction', 'ein_prediction',
+                      'antisemitism_prediction']
+
+        Daily_count_flavors_df = pd.DataFrame()
+        for column in prediction_columns:
+            
+            # Filter DataFrame to get rows where the current column is True, then group by day and get the size of each group
+            true_counts = Jan6[Jan6[column]].groupby('Day').size()
+            Daily_count_flavors_df[column] = true_counts
+
+        SNS_columns  = Jan6['SNS Source'].unique().tolist()
+        Daily_count_sns_df = pd.DataFrame()
+
+        for column in SNS_columns:
+
+            true_counts = Jan6[Jan6['SNS Source'] == column].groupby('Day').size()
+            Daily_count_sns_df[column] = true_counts
+
+                
+        correlations = {}
+        for col1 in Daily_count_flavors_df.columns:
+            for col2 in Daily_count_sns_df.columns:
+                correlation = Daily_count_flavors_df[col1].corr(Daily_count_sns_df[col2])
+                correlations[(col1, col2)] = correlation
+
+
+
+        columns1, columns2 = zip(*correlations.keys())
+
+        # Extract correlation values
+        correlation_values = list(correlations.values())
+
+        # Create a DataFrame from the correlation values with columns as the original column names
+        correlation_df = pd.DataFrame({'Column1': columns1, 'Column2': columns2, 'Correlation': correlation_values})
+
+        # Pivot the DataFrame to create a correlation matrix
+        correlation_matrix = correlation_df.pivot('Column1', 'Column2', 'Correlation')
+
+        # Remove the 'Instagram' column and row from the correlation matrix
+        try:
+            correlation_matrix = correlation_matrix.drop('Instagram', axis=1)
+        except:
+            pass
+
+        # Plot the heatmap
+        fig, ax = plt.subplots(figsize=(10, 6))
+        sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+        ax.set_title('Correlation Heatmap between the count of links and hate type detected across different SNS')
+        st.pyplot(fig)
+
+
+
+
+
+
 
