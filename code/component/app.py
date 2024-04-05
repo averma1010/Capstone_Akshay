23db7170de0d81cc2516192bd6fe00df9819f3c4
Akshay Verma <44718434+averma1010@users.noreply.github.com>
2024-04-05 17:31:21 -0400

    Update app.py

--- code/component/app.py
+++ code/component/app.py
@@ -7,6 +7,8 @@ import network
 import Charts_dashboard
 import numpy as np
 from datetime import datetime, timedelta
+import RAG_Insights
+
 
 
 
@@ -20,31 +22,31 @@ c1, c2, c3 = st.columns([2,2,1])
 with c3:    
     st.markdown('#')
     st.markdown('#')
-    
+    physics_checkbox = st.checkbox('Add Physics(Under Construction)')
+
     st.empty()
     start_date, end_date = st.slider("Select Date Range for Pre Event", 
-                                     min_value=datetime(2021, 1, 1),
+                                     min_value=datetime(2020, 11, 1),
                                      max_value=datetime(2021, 1, 10),
                                      value=(datetime(2021, 1, 1), datetime(2021, 1, 5)),
                                      format="YYYY-MM-DD")
-    network.pre_event(start_date, end_date)
+    
     slider2_key = "slider2"
     
     start_date_1, end_date_1 = st.slider("Select Date Range for Post Event", 
-                                        min_value=datetime(2021, 1, 1),
+                                        min_value=datetime(2020, 11, 1),
                                         max_value=datetime(2021, 1, 10),
                                         value=(datetime(2021, 1, 6), datetime(2021, 1, 10)),
                                         format="YYYY-MM-DD", key=slider2_key)
-    network.post_event( start_date_1, end_date_1)
-
     
-    Charts_dashboard.Network_comparison().network_comparison_df(start_date, end_date, start_date_1, end_date_1)
+    network.network_vis(physics_checkbox,start_date, end_date, start_date_1, end_date_1)
+    
+    Charts_dashboard.Network_comparison().network_comparison_df( start_date, end_date, start_date_1, end_date_1)
 
 with c1:
-    #physics_checkbox = st.sidebar.checkbox('Add Physics')
 
-    
-    st.markdown("## Pre-event")
+   # network.pre_event(start_date, end_date)
+    st.markdown("## Pre-event (Hate Core)")
 
     
     HtmlFile = open("preevent.html", 'r', encoding='utf-8')
@@ -54,9 +56,9 @@ with c1:
     components.html(source_code, height = 500,width=600) 
     
 with c2:
-  
+   # network.post_event( start_date_1, end_date_1)
     
-    st.markdown("## Post-event")
+    st.markdown("## Post-event (Hate Core)")
 
     HtmlFile = open("postevent.html", 'r', encoding='utf-8')
     source_code = HtmlFile.read() 
@@ -79,12 +81,11 @@ c6,c7 = st.columns([1,1])
 
 with c6:
     if st.button("Generate AI Insight"):
-            import RAG_Insights
             data = Charts_dashboard.Network_comparison().network_comparison_metrics(start_date, end_date, start_date_1, end_date_1)
             RAG_Insights.rag_openai(data)
         
 
 with c7:         
-    Charts_dashboard.corr_plot().corr_plot(start_date, end_date_1)
+    Charts_dashboard.corr_plot().correlation_plot(start_date, end_date_1)
 
 
